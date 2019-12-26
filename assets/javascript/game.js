42779bf29e6bef7714a9271bde97de832bf762aa
Mlipphardt <matthewlipphardt@gmail.com>
2019-12-26 16:57:11 -0500

    Added variables for word guess dictionary, grabbing text in html

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var chosenWordText = document.getElementById("chosenWord");
+var guessesRemainingText = document.getElementById("guessesRemaining");
+var winsText = document.getElementById("winsText");
+var lossesText = document.getElementById("lossesText");
+var lettersGuessedText = document.getElementById("lettersGuessed");
+
+var wordDictionary = [
+   "orc",
+   "elf",
+   "wizard",
+   "kobold",
+   "goblin",
+   "paladin",
+   "cleric",
+   "knight" 
+]
+
