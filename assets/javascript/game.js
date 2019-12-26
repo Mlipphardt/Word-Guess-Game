2424ae867f420824f9f5996b138397185644b7a6
Mlipphardt <matthewlipphardt@gmail.com>
2019-12-26 17:14:59 -0500

    Enlarged text for chosen word, added array to display blanks of chosen word to user

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
+
+// Variables for grabbing html tags
 var chosenWordText = document.getElementById("chosenWord");
 var guessesRemainingText = document.getElementById("guessesRemaining");
 var winsText = document.getElementById("winsText");
 var lossesText = document.getElementById("lossesText");
 var lettersGuessedText = document.getElementById("lettersGuessed");
 
+// Possible choices for goal word
 var wordDictionary = [
    "orc",
    "elf",
@@ -13,5 +16,19 @@ var wordDictionary = [
    "paladin",
    "cleric",
    "knight" 
-]
+];
+
+//Selects goal word
+var chosenWord = wordDictionary[Math.floor(Math.random() * wordDictionary.length)];
+
+//Creates underscores for user equal to length of chosen word
+var guessingStatus = [];
+
+for (var i = 0; i < chosenWord.length; i++) {
+    guessingStatus.push("_ ");
+}
+
+chosenWordText.textContent = guessingStatus.join(" ");
+
+console.log(guessingStatus)
 
